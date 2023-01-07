961aa8e26fb15b1271a2cfe6845701f963fd2e70
VALZ-DESKTOP\vanil <vanilatha.se@gmail.com>
2023-01-06 21:44:05 -0800

    program for a primenumber

--- primenumber.py
+++ primenumber.py
@@ -1,3 +1,11 @@
 # this is a program for primenumbers in python
-def primenum(x):
-  pass
+def primenum(n):
+    if n>1:
+        for i in range(2,int(n/2)+1):
+            if (n%i)==0:
+                print(f'{n} is not a prime number')
+                break
+        else:
+            print(f'{n} is  a prime number')
+    else:
+        print(f'{n} is Not a prime number')
